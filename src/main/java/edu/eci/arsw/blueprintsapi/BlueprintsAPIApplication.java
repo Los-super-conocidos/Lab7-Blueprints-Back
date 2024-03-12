c97d0f558002d20e97e13436fda15dd4c9de8342
SebasZaUr <Sebasssele2008@gmail.com>
2024-03-11 22:21:16 -0500

    Arrego para hacerlo rest

--- src/main/java/edu/eci/arsw/blueprintsapi/BlueprintsAPIApplication.java
+++ src/main/java/edu/eci/arsw/blueprintsapi/BlueprintsAPIApplication.java
@@ -2,7 +2,10 @@ package edu.eci.arsw.blueprintsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"edu.eci.arsw.blueprints"})
@@ -11,5 +14,15 @@ public class BlueprintsAPIApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(BlueprintsAPIApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
+			}
+		};
 	}
 }
